0639121e5c29aed373fac3bf7163606e9a178aee
nadayoussef24 <nadayoussef0258@gmail.com>
2025-10-19 00:18:29 +0300

    Initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,34 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { routes } from './app.routes';
+
+export function HttpLoaderFactory() {
+  return new TranslateHttpLoader();
+}
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+
+    provideHttpClient(withInterceptorsFromDi()),
+
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        fallbackLang: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [],
+        },
+      })
+    ),
+  ],
 };
